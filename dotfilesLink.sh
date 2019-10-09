1f8c0cbd029417f288d876f93c5ce95529aa7845
4superu <obarashigeharu@gmail.com>
2019-10-09 10:07:49 +0900

    fix  config link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,6 +6,8 @@ ln -sf ~/dotfiles/nvim/rc/dein_lang.toml ~/.config/nvim/rc/dein_lang.toml
 ln -sf ~/dotfiles/nvim/rc/dein_lazy.toml ~/.config/nvim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/nvim/rc/dein_neo.toml ~/.config/nvim/rc/dein_neo.toml
 ln -sf ~/dotfiles/nvim/rc/dein_python.toml ~/.config/nvim/rc/dein_python.toml
-ln -sf ~/dotfiles/nvim/rc/filetype.vim ~/.config/nvim/rc/filetype.vim
-ln -sf ~/dotfiles/nvim/rc/options.vim ~/.config/nvim/rc/options.vim
-
+ln -sf ~/dotfiles/nvim/rc/dein_ruby.toml ~/.config/nvim/rc/dein_ruby.toml
+ln -sf ~/dotfiles/nvim/rc/dein_clang.toml ~/.config/nvim/rc/dein_clang.toml
+ln -sf ~/dotfiles/nvim/rc/filetype.rc.vim ~/.config/nvim/rc/filetype.rc.vim
+ln -sf ~/dotfiles/nvim/rc/options.rc.vim ~/.config/nvim/rc/options.rc.vim
+ln -sf ~/dotfiles/nvim/rc/command.rc.vim ~/.config/nvim/rc/command.rc.vim
